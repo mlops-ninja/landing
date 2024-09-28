5929824c188a8c922acce8021b0516a1c045cf5b
Dmitry Rubinstein <rubinsteindb@gmail.com>
2024-09-28 13:52:02 +0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Space Grotesk",
+          "Fira Sans",
+          "Bricolage Grotesque Variable",
+          "Inter Variable",
+          "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
