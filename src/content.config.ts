bee525477c2e4dc02c881193a4ccd64c0fa4f32b
Dmitry Rubinstein <rubinsteindb@gmail.com>
2025-06-07 18:25:56 +0300

    Fix font jumping

---
+++ src/content.config.ts
@@ -0,0 +1,77 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection, reference } from 'astro:content';
+
+// 2. Define your collection(s)
+const blog = defineCollection({
+  schema: ({ image }) => z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    snippet: z.string(),
+    image: z.object({
+      src: z.union([z.string(), image()]),
+      alt: z.union([z.string(), image()]),
+    }),
+    publishDate: z.string().transform(str => new Date(str)),
+    author: z.string().default('Astroship'),
+    category: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+const team = defineCollection({
+  schema: ({image}) =>  z.object({
+    draft: z.boolean(),
+    name: z.string(),
+    title: z.string(),
+    avatar: z.object({
+      src: image(),
+      alt: z.string(),
+    }),
+    description: z.string().optional(),
+    publishDate: z.string().transform(str => new Date(str)),
+  }),
+});
+
+const lifecycle = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    icon: z.string(),
+    sections: z.array(reference('section')),
+  }),
+});
+
+const section = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    icon: z.string(),
+  }),
+});
+
+const certification = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    publishDate: z.string().transform(str => new Date(str)).optional(),
+  }),
+});
+
+const products = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    publishDate: z.string().transform(str => new Date(str)).optional(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'blog': blog,
+  'team': team,
+  'lifecycle': lifecycle,
+  'section': section,
+  'certification': certification,
+  'products': products,
+};
---
+++ src/content/team/virviil.md
@@ -0,0 +1,11 @@
+---
+draft: false
+name: "Dmitry Rubinstein"
+title: "MLOps Ninja"
+avatar: {
+    src: "./virviil.jpg",
+    alt: "Dmitry Rubinstein"
+}
+description: "Keks"
+publishDate: "2024-11-14 15:00"
+---
